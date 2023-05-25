cb16634239943612cbd5ae69f3909c82918247f6
Tine-Lovise Storvoll <tinelovisestorvoll@gmail.com>
2023-05-25 13:34:06 +0200

    feat: setup React Router and link views

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./views/Dashboard";
+import Profile from "./views/Profile";
+import Process from "./views/Process";
+import Consultants from "./views/Consultants";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/myprofile" element={<Profile />} />
+          <Route path="/process" element={<Process />} />
+          <Route path="/consultants" element={<Consultants />} />
+        </Routes>
+      </div>
+      ;
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/views/Consultants.jsx
@@ -0,0 +1,7 @@
+const Consultants = () => {
+    return (
+        <h1>Consultants</h1>
+    )
+}
+
+export default Consultants;
